1972b44b6a568f1c403e26e398d3b9dc833ccf99
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-20 21:33:01 -0500

    agregar funcion de salvar nuevo evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onSetActiveEvent } from '../store/calendar/calendarSlice';
+import {
+  onAddNewEvent,
+  onSetActiveEvent,
+  onUpdateEvent,
+} from '../store/calendar/calendarSlice';
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
@@ -9,6 +13,19 @@ export const useCalendarStore = () => {
     dispatch(onSetActiveEvent(calendarEvent));
   };
 
+  const startSavingEvent = async (calendarEvent) => {
+    // TODO: llegar al backend
+
+    // Todo bien
+    if (calendarEvent._id) {
+      // Actualizado
+      dispatch(onUpdateEvent({ ...calendarEvent }));
+    } else {
+      // Creando
+      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+    }
+  };
+
   return {
     // Propiedades
     events,
@@ -16,5 +33,6 @@ export const useCalendarStore = () => {
 
     // Métodos
     setActiveEvent,
+    startSavingEvent,
   };
 };
