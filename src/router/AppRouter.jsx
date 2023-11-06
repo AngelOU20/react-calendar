d32e2bf26e98e4b85be0186a0a9e8635e90bc37a
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-06 18:36:45 -0500

    add useAuthStore in AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,24 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { CalendarPage } from '../calendar';
 import { LoginPage } from '../auth';
+import { useAuthStore } from '../hooks';
+import { useEffect } from 'react';
 
 export const AppRouter = () => {
-  const authStatus = 'not-authenticated'; // 'authenticated' and 'not-authenticated'
+  // const authStatus = 'not-authenticated'; // 'authenticated' and 'not-authenticated'
+  const { status, checkAuthToken } = useAuthStore();
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
+  if (status === 'checking') {
+    return <h3>Cargando...</h3>;
+  }
 
   return (
     <Routes>
-      {authStatus === 'not-authenticated' ? (
+      {status === 'not-authenticated' ? (
         <Route path="/auth/*" element={<LoginPage />} />
       ) : (
         <Route path="/*" element={<CalendarPage />} />
