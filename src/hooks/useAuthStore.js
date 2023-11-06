2338b46b09a7da3ffa9a6a3625c68190b26c3c44
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-06 17:11:32 -0500

    include feature startLogin

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,16 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import calendarApi from '../api/calendarApi';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store';
 
 export const useAuthStore = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const { status, user, errorMessage } = useSelector((state) => state.auth);
 
   const startLogin = async ({ email, password }) => {
+    dispatch(onChecking());
     try {
-      const resp = await calendarApi.post('/auth', { email, password });
-      console.log(resp);
+      const { data } = await calendarApi.post('/auth', { email, password });
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
-      console.log(error);
+      dispatch(onLogout('Credenciales incorrectas'));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
     }
   };
 
