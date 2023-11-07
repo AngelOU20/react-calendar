6caee961f495478052067d1279679b426133ed65
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-06 22:01:43 -0500

    include username and startLogout in Navbar

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { useAuthStore } from '../../hooks';
+
 export const Navbar = () => {
+  const { startLogout, user } = useAuthStore();
+
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4 py-2">
-      <span className="navbar-brand text-white">
+      <span className="navbar-brand text-white text-capitalize">
         <i className="fas fa-calendar-alt"></i>
-        &nbsp; Julio Ucharima
+        &nbsp; {user.name}
       </span>
 
-      <button className="btn btn-outline-danger">
+      <button className="btn btn-outline-danger" onClick={startLogout}>
         <i className="fa-solid fa-right-from-bracket"></i>
         &nbsp;
         <span>Salir</span>
